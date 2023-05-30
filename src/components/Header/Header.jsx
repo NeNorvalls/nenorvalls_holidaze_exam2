e4e1b0dc25c0850834fb1ac75085ebdc90fd4b0e
Crystaliner1000 <honeygeeh27@gmail.com>
2023-05-30 21:09:23 +0200

    Fixing error in the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,8 @@ import Nav from 'react-bootstrap/Nav'
 import './Header.css'
 
 const Header = () => {
+  const isLoggedIn = localStorage.getItem('isLoggedIn')
+
   const handleLogout = () => {
     localStorage.removeItem('isLoggedIn')
     localStorage.removeItem('username')
@@ -18,10 +20,12 @@ const Header = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="/">Dashboard</Nav.Link>
-          <Nav.Link href="/logout" onClick={handleLogout}>
-            Logout
-          </Nav.Link>
+          {!isLoggedIn && <Nav.Link href="/">Dashboard</Nav.Link>}
+          {isLoggedIn && (
+            <Nav.Link href="/logout" onClick={handleLogout}>
+              Logout
+            </Nav.Link>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
